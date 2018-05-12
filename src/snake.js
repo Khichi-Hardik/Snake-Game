12e1c4789891196894382bef9c7d0fe7f3fd28bf
Khichi-Hardik <hardikkhichi@gmail.com>
2018-05-13 00:47:21 +0530

    food added

--- src/snake.js
+++ src/snake.js
@@ -1,18 +1,24 @@
-var snake
+var px = 40;
+var snakeSpeed = 10;
+var s;
+var f;
 var dead = false;
+var eaten = false;
+var count = 0;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	snake = new snake();
+	s = new snake();
+	f = new food();
 }
 
 function draw() {
-	frameRate(20);
+	frameRate(snakeSpeed);
 	background(0);
 
 	//To make Grid
-	for (var x = 0; x < windowWidth; x += 20) {
-		for (var y = 0; y < windowHeight; y += 20) {
+	for (var x = 0; x <= windowWidth; x += px) {
+		for (var y = 0; y <= windowHeight; y +=px) {
 			stroke(100);
 			strokeWeight(1);
 			line(x, 0, x, height);
@@ -20,45 +26,77 @@ function draw() {
 		}
 	}
 	if(dead == false){
-		snake.display();
-		snake.move();
-		snake.isDead();
+		f.display();
+
+		s.display();
+		s.move();
+		s.isDead();
+		s.eat(f);
 	}
 	else{
 		fill(255);
 		textSize(60);
 		text("Snake is Dead",20, 60);
+		text("Your Score is : "+count,20,120);
+	}
+
+	if (eaten == true){
+		f = new food;
+		eaten =false;
 	}
 }
 
 function keyPressed(){
 	if (keyCode == UP_ARROW){
-		snake.velocity = createVector(0,-20);
+		s.velocity = createVector(0,-px);
 	}
 	else if (keyCode == DOWN_ARROW){
-		snake.velocity = createVector(0,20);
+		s.velocity = createVector(0,px);
 	}
 	else if (keyCode == LEFT_ARROW){
-		snake.velocity = createVector(-20,0);
+		s.velocity = createVector(-px,0);
 	}
 	else if (keyCode == RIGHT_ARROW){
-		snake.velocity = createVector(20,0);
+		s.velocity = createVector(px,0);
 	}
 }
 
 function snake(){
 	this.position = createVector(0,0);
-	this.velocity = createVector(20,0);
+	this.velocity = createVector(px,0);
+}
+function food(){
+	this.position = roundOff(createVector(random(0,windowWidth), random(0,windowHeight)));
 }
 
+
 snake.prototype.display = function(){
-	rect(this.position.x, this.position.y, 20, 20);
+	fill(255);
+	rect(this.position.x, this.position.y, px, px);
 }
 snake.prototype.move = function(){
 	this.position.add(this.velocity);
 }
-
 snake.prototype.isDead = function(){
 	if(this.position.x < 0 || this.position.x > width){dead = true;}
-    if(this.position.y < 0 || this.position.y > height){dead = true;}
+    else if(this.position.y < 0 || this.position.y > height){dead = true;}
+}
+snake.prototype.eat = function(f){
+	if((this.position.x == f.position.x)&&(this.position.y == f.position.y)){
+		eaten = true;
+		count+=1;
+	}
+}
+
+
+food.prototype.display = function(){
+	fill(255,255,0);
+	rect(this.position.x, this.position.y, px, px);
+}
+
+
+function roundOff(n){
+	n.x = n.x - (n.x % px);
+	n.y = n.y - (n.y % px);
+	return createVector(n.x,n.y);
 }
